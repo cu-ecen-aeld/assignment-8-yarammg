de21921b56476ab735992c801e665b908078b70a
yarammg <yara.gowayed@etas.com>
2025-03-24 23:35:31 +0100

    Assignment 7 complete

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -9,7 +9,6 @@ LDD_VERSION = 5c3cae6ddc96b8645dfa6f6bc4ddbba08aae8789
 LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-yarammg.git
 LDD_SITE_METHOD = git
 
-
 LDD_MODULE_SUBDIRS = misc-modules scull
 
 $(eval $(generic-package))
